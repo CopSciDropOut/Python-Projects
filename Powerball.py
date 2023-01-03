a017c5d8d31680631ae9c574fc3fbcd98002e495
CompSciDropOut <112856722+CopSciDropOut@users.noreply.github.com>
2023-01-03 04:59:45 -0500

    Update Powerball.py

--- Powerball.py
+++ Powerball.py
@@ -1,7 +1,9 @@
 import random
-for numbers in range(5):
-    white_numbers = random.randint(1, 70)
-    print(f'White numbers: {white_numbers}')
-red_number = random.randint(1, 27)
-print(f'Red number: {red_number}')
-    
+again = 'y'
+while again == 'y' or again == 'Y':
+    for numbers in range(5):
+        white_numbers = random.randint(1, 70)
+        print(f'White numbers: {white_numbers}')
+    red_number = random.randint(1, 27)
+    print(f'Red number: {red_number}')
+    again = input("Generate again?: ")
